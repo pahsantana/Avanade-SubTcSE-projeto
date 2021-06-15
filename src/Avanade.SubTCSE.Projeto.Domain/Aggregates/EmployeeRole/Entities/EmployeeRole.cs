4ebca2b4478f582d82ed24926b8bd575540cfc21
Paloma Santana <santanagilberto@uol.com.br>
2021-06-15 08:58:53 -0300

    Aula 3 - Solid - Open and close principle

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,20 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public class EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; private set; }
+
+        public string RoleName { get; private set; }
+
+        public void AddRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+    }
+}
