38a0b54ecbab97c28c5a99c89883f74044dec2b6
Paloma Santana <santanagilberto@uol.com.br>
2021-06-17 08:59:34 -0300

    Aula 4 - SOLID - Liskov

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Services/IEmployeeRoleService.cs
@@ -0,0 +1,8 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services
+{
+    interface IEmployeeRoleService
+    {
+        void AddEmployeeRole(Entities.EmployeeRole employeeRole);
+
+    }
+}
