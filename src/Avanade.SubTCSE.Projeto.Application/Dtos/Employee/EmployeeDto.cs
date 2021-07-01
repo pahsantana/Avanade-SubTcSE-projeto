db87549dbf7978ada2e9074a610859837075d942
Paloma Santana <santanagilberto@uol.com.br>
2021-07-01 01:55:16 -0300

    Aula 7

---
+++ src/Avanade.SubTCSE.Projeto.Application/Dtos/Employee/EmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿using Avanade.SubTCSE.Projeto.Application.Dtos.Base;
+using System;
+
+namespace Avanade.SubTCSE.Projeto.Application.Dtos.Employee
+{
+    public class EmployeeDto : BaseDto
+    {
+        public string PrimeiroNome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public DateTime Aniversario { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public decimal Salario { get; init; }
+
+        public EmployeeRole.EmployeeRoleDto Cargo { get; init; }
+    }
+}
